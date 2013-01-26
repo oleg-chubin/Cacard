16d27557a42975aea4f6e68aa2937c462643fe70
Andrew <andrew@andrew-X51RL.(none)>
2013-01-27 00:16:07 +0200

    create models for info< translate< adress and news

--- src/Cacard/Cacard/urls.py
+++ src/Cacard/Cacard/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+                       
     # Examples:
     # url(r'^$', 'Cacard.views.home', name='home'),
     # url(r'^Cacard/', include('Cacard.foo.urls')),
